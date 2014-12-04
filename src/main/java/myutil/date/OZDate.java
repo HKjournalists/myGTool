d03576115fd6ebf344cbf09d480470cb1f016ff7
ouzhou8866 <245508489@qq.com>
2014-12-04 11:49:38 +0800

    1

--- src/main/java/myutil/date/OZDate.java
+++ src/main/java/myutil/date/OZDate.java
@@ -15,10 +15,14 @@ public class OZDate {
 	 * 													  2、yyyyMMddHHmmss
 	 * @return 字符串类型
 	 */
-	public String ozGetDate(String dateForm){
+	public static String ozGetDate(String dateForm){
 		SimpleDateFormat df=new SimpleDateFormat(dateForm);
 		String dateStr = df.format(new Date());
 		return dateStr;
 	}
 	
+	
+	public static void main(String[] args) {
+		System.out.println(ozGetDate("yyyy_MM_dd"));
+	}
 }
