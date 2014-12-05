fcb71bd71c7b42d4572b434beba16f7f8006d2ed
ouzhou8866 <245508489@qq.com>
2014-12-05 11:56:24 +0800

    date中增加了时间类型转换

--- src/main/java/myutil/date/OZDate.java
+++ src/main/java/myutil/date/OZDate.java
@@ -1,5 +1,6 @@
 package myutil.date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -20,6 +21,32 @@ public class OZDate {
 		String dateStr = df.format(new Date());
 		return dateStr;
 	}
+	
+	/**
+	 * 将 String 类型的时间转化为 Date 类型<br>
+	 * ps:参考地址  http://liulinxia02.blog.163.com/blog/static/2686877201142322741461/
+	 * @date 2014-12-4
+	 */
+	public static void ozStrToDate(){
+		try {
+			String sDt = "12/28/2014";
+			SimpleDateFormat sdf= new SimpleDateFormat("MM/dd/yyyy");
+			Date dt2 = sdf.parse(sDt);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 将date转为long类型
+	 * @date 2014-12-4
+	 */
+	public static void ozDateToLong(){
+		Date today = new Date();
+		long time = today.getTime();
+	}
+	
 	
 	
 	public static void main(String[] args) {
