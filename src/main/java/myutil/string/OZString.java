05e359f99aaa3dc1e6f2ffba88aac273aa74966a
ouzhou8866 <245508489@qq.com>
2014-12-21 15:40:46 +0800

    String用法总结

---
+++ src/main/java/myutil/string/OZString.java
@@ -0,0 +1,31 @@
+package myutil.string;
+
+/**
+ * 对String用法的一些总结
+ * @author Europe
+ */
+public class OZString {
+
+	/**
+	 * int indexOf(int ch)   返回指定字符在此字符串中第一次出现处的索引。 
+	 * int indexOf(int ch, int fromIndex) 从指定的索引开始搜索，返回在此字符串中第一次出现指定字符处的索引。 
+	 * int indexOf(String str) 返回第一次出现的指定子字符串在此字符串中的索引。 
+	 * int indexOf(String str, int fromIndex) 从指定的索引处开始，返回第一次出现的指定子字符串在此字符串中的索引。 
+	 */
+	public void ozStrigIndexOf(){
+		
+		String str = "ba";
+		int a = str.indexOf("a");
+		System.out.println(a);
+		
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new OZString().ozStrigIndexOf();
+	}
+	
+}
