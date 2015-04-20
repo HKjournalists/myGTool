f32fcabc636bb71cf5c9d8b1a543e14aabddea64
ouzhou8866 <245508489@qq.com>
2015-04-20 11:57:10 +0800

    增加了socket

--- src/main/java/myutil/socket/OZSocketTool.java
+++ src/main/java/myutil/socket/OZSocketTool.java
@@ -4,20 +4,35 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 /**
  * 一个建立socket服务端的实例
+ * 
  * @author Europe
  */
 public class OZSocketTool {
 
+	/*
+	 * ********* Socket 精要所在 **********
+	 * 
+	 * Socket socket = new Socket("192.168.0.59", 3000);
+	 * 
+	 * PrintWriter pw = new PrintWriter(new
+	 * BufferedWriter(newOutputStreamWriter(
+	 * socket.getOutputStream())),true);//构建向服务端请求对象 pw.println(“str”);//向服务端发送
+	 * 请求 str
+	 * 
+	 * BufferedReader br = new
+	 * BufferedReader(newInputStreamReader(socket.getInputStream
+	 * (),"GBK"));//读入对象
+	 * 
+	 * String data = br.readLine();//获得服务端返回的字符串
+	 */
+
 	public static void main(String[] args) {
-		//启动方式
+		// 启动方式
 		new Thread(new OZSocketTool().new StartSocketThreed()).start();
 	}
-	
-	
-	
+
 	/**
 	 * 用来启动 socket 线程的线程
 	 * 
@@ -40,10 +55,9 @@ public class OZSocketTool {
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * 1、启动socket线程服务<br>
 	 * 2、接到请求开始截图
@@ -68,8 +82,7 @@ public class OZSocketTool {
 
 			try {
 				System.out.println("已接收一个客户端请求");
-				
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
@@ -77,7 +90,5 @@ public class OZSocketTool {
 		}
 
 	}
-	
-	
-	
+
 }
