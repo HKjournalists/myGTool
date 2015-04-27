99120da659d8a55d0452eb4109e8831d0f075de4
ouzhou8866 <245508489@qq.com>
2015-04-27 10:56:08 +0800

    添加python

---
+++ src/main/java/myutil/python/PythonMethod.java
@@ -0,0 +1,21 @@
+package myutil.python;
+
+public class PythonMethod {
+
+	//声明变量
+//	movies = ["The meaning Life",1983,"The first Age",1945]
+	
+	
+	/**======================= 循环 ========================*/
+	//for循环,打印出movies中的每一项
+//	for each_item in movies:print(each_item)
+	
+	//while循环
+//	count = 0
+//	while count<len(movies):
+//		print(movies[count])
+//		count = count+1
+	
+	
+	
+}
